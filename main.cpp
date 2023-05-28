0a5557e91474387b358189c5605c81ee4eb4a8a7
AmemiyaRei <summerwind1228@163.com>
2023-05-28 20:24:21 +0800

    wire mesh

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include "tgaimage.h"
+#include <vector>
+#include "geometry.h"
+#include "model.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+Model *model = NULL;
+const int width = 800;
+const int height = 800;
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+    bool steep = false;
+    if (std::abs(x0 - x1) < abs(y0 - y1)) {
+        steep = true;
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+    }
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int derror2 = std::abs(dy) * 2;
+    int error2 = 0;
+    int y = y0;
+    for (int x = x0; x <= x1; ++x) {
+        if (steep) image.set(y, x, color);
+        else image.set(x, y, color);
+        error2 += derror2;
+        if (error2 > dx) {
+            y += (y1 > y0 ? 1 : -1);
+            error2 -= dx * 2;
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    if(argc == 2) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("../obj/african_head.obj");
+    }
+
+    TGAImage image(width, height, TGAImage::RGB);
+    for (int i = 0; i < model->nfaces(); ++i) {
+        std::vector<int> face = model->face(i);
+        for (int j = 0; j < 3; ++j) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j + 1) % 3]);
+            // map x, y to image, -1.0 <= x, y <= 1.0
+            int x0 = (v0.x + 1.0) * width / 2.0;
+            int y0 = (v0.y + 1.0) * height / 2.0;
+            int x1 = (v1.x + 1.0) * width / 2.0;
+            int y1 = (v1.y + 1.0) * height / 2.0;
+            if (v0.z + v1.z > 0)
+                line(x0, y0, x1, y1, image, red);
+            else line(x0, y0, x1, y1, image, white);
+
+        }
+    }
+    image.flip_vertically();
+    image.write_tga_file("output.tga");
+    delete model;
+    return 0;
+}
