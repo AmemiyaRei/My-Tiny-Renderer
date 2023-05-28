c0299b08b2b7e2613440f6618cc9a7fcc2e304c1
AmemiyaRei <summerwind1228@163.com>
2023-05-28 21:24:57 +0800

    branch triangle init

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 Model *model = NULL;
-const int width = 800;
-const int height = 800;
+const int width = 200;
+const int height = 200;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     bool steep = false;
@@ -37,6 +38,12 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     }
 }
 
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+    line(t0.x, t0.y, t1.x, t1.y, image, color);
+    line(t0.x, t0.y, t2.x, t2.y, image, color);
+    line(t2.x, t2.y, t1.x, t1.y, image, color);
+}
+
 int main(int argc, char** argv) {
     if(argc == 2) {
         model = new Model(argv[1]);
@@ -45,22 +52,29 @@ int main(int argc, char** argv) {
     }
 
     TGAImage image(width, height, TGAImage::RGB);
-    for (int i = 0; i < model->nfaces(); ++i) {
-        std::vector<int> face = model->face(i);
-        for (int j = 0; j < 3; ++j) {
-            Vec3f v0 = model->vert(face[j]);
-            Vec3f v1 = model->vert(face[(j + 1) % 3]);
-            // map x, y to image, -1.0 <= x, y <= 1.0
-            int x0 = (v0.x + 1.0) * width / 2.0;
-            int y0 = (v0.y + 1.0) * height / 2.0;
-            int x1 = (v1.x + 1.0) * width / 2.0;
-            int y1 = (v1.y + 1.0) * height / 2.0;
-            if (v0.z + v1.z > 0)
-                line(x0, y0, x1, y1, image, red);
-            else line(x0, y0, x1, y1, image, white);
+//    for (int i = 0; i < model->nfaces(); ++i) {
+//        std::vector<int> face = model->face(i);
+//        for (int j = 0; j < 3; ++j) {
+//            Vec3f v0 = model->vert(face[j]);
+//            Vec3f v1 = model->vert(face[(j + 1) % 3]);
+//            // map x, y to image, -1.0 <= x, y <= 1.0
+//            int x0 = (v0.x + 1.0) * width / 2.0;
+//            int y0 = (v0.y + 1.0) * height / 2.0;
+//            int x1 = (v1.x + 1.0) * width / 2.0;
+//            int y1 = (v1.y + 1.0) * height / 2.0;
+//            if (v0.z + v1.z > 0)
+//                line(x0, y0, x1, y1, image, red);
+//            else line(x0, y0, x1, y1, image, white);
+//
+//        }
+//    }
 
-        }
-    }
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
     image.flip_vertically();
     image.write_tga_file("output.tga");
     delete model;
