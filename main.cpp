29d7805d79b84da70c6796d31fdf87e229799519
AmemiyaRei <summerwind1228@163.com>
2023-05-30 23:15:17 +0800

    face light intensity render

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
 Model *model = NULL;
-const int width = 200;
-const int height = 200;
+const int width = 800;
+const int height = 800;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     bool steep = false;
@@ -73,29 +73,37 @@ int main(int argc, char** argv) {
     }
 
     TGAImage image(width, height, TGAImage::RGB);
-//    for (int i = 0; i < model->nfaces(); ++i) {
-//        std::vector<int> face = model->face(i);
-//        for (int j = 0; j < 3; ++j) {
-//            Vec3f v0 = model->vert(face[j]);
-//            Vec3f v1 = model->vert(face[(j + 1) % 3]);
-//            // map x, y to image, -1.0 <= x, y <= 1.0
-//            int x0 = (v0.x + 1.0) * width / 2.0;
-//            int y0 = (v0.y + 1.0) * height / 2.0;
-//            int x1 = (v1.x + 1.0) * width / 2.0;
-//            int y1 = (v1.y + 1.0) * height / 2.0;
-//            if (v0.z + v1.z > 0)
-//                line(x0, y0, x1, y1, image, red);
-//            else line(x0, y0, x1, y1, image, white);
-//
-//        }
-//    }
+    // light direction: -z
+    Vec3f light_dir(0, 0, -1);
 
-    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
-    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
-    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
-    triangle(t0[0], t0[1], t0[2], image, red);
-    triangle(t1[0], t1[1], t1[2], image, white);
-    triangle(t2[0], t2[1], t2[2], image, green);
+    for (int i = 0; i < model->nfaces(); ++i) {
+        std::vector<int> face = model->face(i);
+        Vec3f world_coords[3];
+        Vec2i screen_coords[3];
+        for (int j = 0; j < 3; ++j) {
+            Vec3f v = model->vert(face[j]);
+            // scaled 2D projection coordinates in image
+            screen_coords[j] = Vec2i((v.x + 1.0) * width / 2.0,
+                                     (v.y + 1.0) * height / 2.0);
+            // origin 3D coordinates of the model
+            world_coords[j] = v;
+        }
+        Vec3f n = (world_coords[2] - world_coords[1]) ^ (world_coords[1] - world_coords[0]);
+        n.normalize();
+        // light intensity
+        float intensity = n * light_dir;
+        if (intensity > 0) {
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image,
+                     TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+        }
+    }
+
+//    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+//    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+//    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+//    triangle(t0[0], t0[1], t0[2], image, red);
+//    triangle(t1[0], t1[1], t1[2], image, white);
+//    triangle(t2[0], t2[1], t2[2], image, green);
     image.flip_vertically();
     image.write_tga_file("output.tga");
     delete model;
